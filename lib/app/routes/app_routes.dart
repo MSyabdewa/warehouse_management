a44de2f15c3df65277d3a6778de910fd18530d1d
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-05-09 17:37:05 +0700

    feat: Add products route and view

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -6,6 +6,7 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const LOGIN = _Paths.LOGIN;
   static const ADD_PRODUCT = _Paths.ADD_PRODUCT;
+  static const PRODUCTS = _Paths.PRODUCTS;
 }
 
 abstract class _Paths {
@@ -13,4 +14,5 @@ abstract class _Paths {
   static const HOME = '/home';
   static const LOGIN = '/login';
   static const ADD_PRODUCT = '/add-product';
+  static const PRODUCTS = '/products';
 }
