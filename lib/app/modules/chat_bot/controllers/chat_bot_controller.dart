81f30395b40828b8cabfe9717e0e42d9429c9b8d
Muhammad Syabdewa <syabdewa02@gmail.com>
2024-05-15 03:20:13 +0700

    feat: Integrate OpenAI chatbot API for AI-powered conversation in ChatBotView

--- lib/app/modules/chat_bot/controllers/chat_bot_controller.dart
+++ lib/app/modules/chat_bot/controllers/chat_bot_controller.dart
@@ -1,3 +1,70 @@
-import 'package:get/get.dart';
+import 'dart:convert';
 
-class ChatBotController extends GetxController {}
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+import '../env/constants/open_ai.dart';
+
+class ChatBotController extends GetxController {
+  TextEditingController userInput = TextEditingController();
+  List<ChatMessage> responses = [];
+
+  ChatUser myself = ChatUser(id: '1', firstName: 'You');
+  ChatUser bot = ChatUser(id: '2', firstName: 'AI');
+
+  submitForm(ChatMessage message) async {
+    try {
+      responses.insert(0, message);
+
+      const apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $apiKey'
+        },
+        body: jsonEncode(
+          <String, dynamic>{
+            "model": "gpt-3.5-turbo",
+            "messages": [
+              {
+                "role": "user",
+                "content":
+                    "Jika ada user yang menanyakan kamu siapa atau memberikan ucapan halo, kamu adalah AI yang membantu user untuk mengetahui tentang manajemen gudang (warehouse management) dan juga detail mengenai hal tersebut. Jika pertanyaan di luar dari topik manajemen gudang, tolak dengan halus. Saya ingin memahami lebih dalam tentang manajemen gudang. Bisakah Anda memberikan penjelasan terkait dengan pertanyaan ini: ${message.text}",
+              }
+            ],
+            "max_tokens": 1000,
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        if (responseData['choices'] != null &&
+            responseData['choices'].isNotEmpty) {
+          final bestResponse = responseData['choices'][0]['message']['content'];
+
+          responses.insert(
+            0,
+            ChatMessage(
+              text: bestResponse,
+              user: bot,
+              createdAt: DateTime.now(),
+            ),
+          );
+        } else {
+          print('Gagal mendapatkan respons dari model.');
+        }
+      } else {
+        print('Gagal mendapatkan respons. Kode status: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Terjadi kesalahan: $e');
+    }
+    userInput.clear();
+    update();
+  }
+}
--- lib/app/modules/chat_bot/views/chat_bot_view.dart
+++ lib/app/modules/chat_bot/views/chat_bot_view.dart
@@ -1,23 +1,93 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import '../../../utils/color_palette.dart';
 import '../controllers/chat_bot_controller.dart';
 
 class ChatBotView extends GetView<ChatBotController> {
-  const ChatBotView({Key? key}) : super(key: key);
+  const ChatBotView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChatBotView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ChatBotView is working',
-          style: TextStyle(fontSize: 20),
+        backgroundColor: ColorPalette.pacificBlue,
+        elevation: 0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(16),
+            bottomRight: Radius.circular(16),
+          ),
         ),
+        leading: IconButton(
+          icon: const Icon(Icons.chevron_left_rounded, size: 35),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: const Text(
+          "AI Warehouse Management",
+          style: TextStyle(
+            fontSize: 24,
+            color: ColorPalette.timberGreen,
+          ),
+        ),
+      ),
+      body: GetBuilder<ChatBotController>(
+        builder: (controller) {
+          return Column(
+            children: [
+              Expanded(
+                child: DashChat(
+                  currentUser: controller.myself,
+                  messages: controller.responses,
+                  messageOptions: const MessageOptions(
+                    messagePadding: EdgeInsets.all(12),
+                    containerColor: ColorPalette.pacificBlue,
+                    showTime: true,
+                  ),
+                  inputOptions: InputOptions(
+                    autocorrect: false,
+                    inputDecoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                          color: ColorPalette.pacificBlue,
+                          width: 2,
+                        ),
+                      ),
+                      label: const Text('Message AI...'),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                    ),
+                  ),
+                  onSend: (message) {
+                    controller.submitForm(message);
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Powered by AI',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: ColorPalette.timberGreen,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
